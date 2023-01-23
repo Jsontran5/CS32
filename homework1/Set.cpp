9e791aec6c637d9ae11ab78a1780464117e510bc
Jason Tran <jsontran5@gmail.com>
2023-01-23 02:40:59 -0800

    2:40 am 1/23/23

--- homework1/Set.cpp
+++ homework1/Set.cpp
@@ -1,7 +1,134 @@
 #include "Set.h"
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 Set::Set()
 {
+	m_size = 0;
+}
 
+
+bool Set::empty()
+{
+	if (m_size == 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+int Set::size()
+{
+
+	return m_size;
+}
+
+bool Set::insert(const ItemType& value)
+{
+	if (m_size >= 160)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < m_size; i++)
+	{
+		if (m_words[i] == value)
+		{
+			return false;
+		}
+	}
+
+	m_words[m_size] = value;
+	m_size++;
+
+
+	for (int i = 0; i < m_size; i++) //loop through the array
+	{
+		string temp;
+		//will prevent out of bounds
+		for (int j = i + 1; j < m_size; j++) //will be the value ahead of the array by one.
+		{
+			if (m_words[i] > m_words[j]) //if the element before is greater
+			{
+				temp = m_words[i]; //store the greater element
+				m_words[i] = m_words[j]; //swap the element to the one next to it because it will be smaller.
+				m_words[j] = temp; //the one next to it will now have the greater value
+			}
+		}
+	}
+	return true;
+
+}
+
+bool Set::erase(const ItemType& value)
+{
+	bool present = false;
+	int loc = 0;
+
+	for (int i = 0; i < m_size; i++)
+	{
+		
+		if (m_words[i] == value)
+		{
+			present = true;
+			break;
+		}
+		loc++;
+	}
+
+	if (present == true)//check out of bounds
+	{
+		for (int i = loc; i < m_size; i++)
+		{
+			if (i != m_size - 1)
+			{
+				m_words[i] = m_words[i + 1];
+			}
+		}
+		m_size--;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+bool Set::contains(const ItemType& value) const
+{
+	for (int i = 0; i < m_size; i++)
+	{
+		if (m_words[i] == value)
+		{
+			return true;
+		}
+
+		return false;
+	}
+}
+
+bool Set::get(int i, ItemType& value) const
+{
+	if (0 <= i && i < m_size)
+	{
+		if (i == 0)
+		{
+			value = m_words[0];
+		}
+		else if (i == m_size - 1)
+		{
+			value = m_words[m_size - 1];
+		}
+		else
+		{
+			value = m_words[i];
+		}
+		return true;
+	}
+	return false;
 }
--- homework1/Set.h
+++ homework1/Set.h
@@ -3,7 +3,8 @@
 
 #include <string>
 
-
+using ItemType = std::string;
+const int DEFAULT_MAX_ITEMS = 160;
 
 class Set
 {
@@ -14,22 +15,22 @@ public:
 
     int size();    // Return the number of items in the set.
 
-    bool insert(const std::string& value);
+    bool insert(const ItemType& value);
     // Insert value into the set if it is not already present.  Return
     // true if the value is actually inserted.  Leave the set unchanged
     // and return false if value is not inserted (perhaps because it
     // was already in the set or because the set has a fixed capacity and
     // is full).
 
-    bool erase(const std::string& value);
+    bool erase(const ItemType& value);
     // Remove the value from the set if it is present.  Return true if the
     // value was removed; otherwise, leave the set unchanged and
     // return false.
 
-    bool contains(const std::string& value);
+    bool contains(const ItemType& value) const;
     // Return true if the value is in the set, otherwise false.
 
-    bool get(int i, std::string& value);
+    bool get(int i, ItemType& value) const;
     // If 0 <= i < size(), copy into value the item in the set that is
     // strictly greater than exactly i items in the set and return true.
     // Otherwise, leave value unchanged and return false.
@@ -40,8 +41,9 @@ public:
 
 
 private:
-    int m_count;
-    std::string words[250];
+    
+    ItemType m_words[DEFAULT_MAX_ITEMS];
+    int m_size;
 };
 
 
