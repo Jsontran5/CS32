ed7556eeb4559921544bf3698a51a09b18a788b6
Jason Tran <jsontran5@gmail.com>
2023-02-08 22:02:57 -0800

    10:02 pm 2/8/23

--- cs32HW2FINAL/eval.cpp
+++ cs32HW2FINAL/eval.cpp
@@ -299,8 +299,8 @@ bool isvalidInfix(string infix)
 
 int main()
 {
-	string trueChars = "ucla";
-	string falseChars = "nsx";
+	string trueChars = "tywz";
+	string falseChars = "fnx";
 	Set trues;
 	Set falses;
 	for (int k = 0; k < trueChars.size(); k++)
@@ -310,7 +310,27 @@ int main()
 
 	string pf;
 	bool answer;
-	assert(evaluate("a & !s", trues, falses, pf, answer) == 0 && answer);
-
+	assert(evaluate("w| f", trues, falses, pf, answer) == 0 && pf == "wf|" && answer);
+	assert(evaluate("y|", trues, falses, pf, answer) == 1);
+	assert(evaluate("n t", trues, falses, pf, answer) == 1);
+	assert(evaluate("nt", trues, falses, pf, answer) == 1);
+	assert(evaluate("()", trues, falses, pf, answer) == 1);
+	assert(evaluate("()z", trues, falses, pf, answer) == 1);
+	assert(evaluate("y(n|y)", trues, falses, pf, answer) == 1);
+	assert(evaluate("t(&n)", trues, falses, pf, answer) == 1);
+	assert(evaluate("(n&(t|y)", trues, falses, pf, answer) == 1);
+	assert(evaluate("n+y", trues, falses, pf, answer) == 1);
+	assert(evaluate("", trues, falses, pf, answer) == 1);
+	assert(evaluate("f  |  !f & (t&n) ", trues, falses, pf, answer) == 0
+		&& pf == "ff!tn&&|" && !answer);
+	assert(evaluate(" x  ", trues, falses, pf, answer) == 0 && pf == "x" && !answer);
+	trues.insert('x');
+	assert(evaluate("((x))", trues, falses, pf, answer) == 3);
+	falses.erase('x');
+	assert(evaluate("((x))", trues, falses, pf, answer) == 0 && pf == "x" && answer);
+	trues.erase('w');
+	assert(evaluate("w| f", trues, falses, pf, answer) == 2);
+	falses.insert('w');
+	assert(evaluate("w| f", trues, falses, pf, answer) == 0 && pf == "wf|" && !answer);
 	cout << "Passed all tests" << endl;
 }
